e6a92ed08b28ed75a71b0ddb2bdf86b2b0c01bdf
tranthaitung <77220860+tranthaitung@users.noreply.github.com>
2021-04-26 11:23:52 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import TodoList from "./todo/index";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h2>Hello CodeSandbox</h2>
+    </div>
+  );
+}
