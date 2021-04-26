fbe0a30766caf00427132cecc058521499592de1
tranthaitung <77220860+tranthaitung@users.noreply.github.com>
2021-04-26 13:04:26 +0700

    fix

--- src/sagas.js
+++ src/sagas.js
@@ -1,11 +1,7 @@
-import { all } from 'redux-saga/effects'
-import { authSagas } from './auth';
-import { taskSagas } from './tasks';
-
+import { all } from "redux-saga/effects";
+import { authSagas } from "./auth";
+import { taskSagas } from "./tasks";
 
 export default function* sagas() {
-  yield all([
-    ...authSagas,
-    ...taskSagas
-  ]);
+  yield all([...taskSagas]);
 }
